831503bedffec97009258dfcc0a4de3f19b668ca
Mashood Alam <alammashood123@gmail.com>
2026-07-24 02:07:49 +0500

    Fix frontend App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/DashBoard";
-import Transactions from "./pages/Transactions";
+import Transactions from "./pages/Transaction";
 import Profile from "./pages/Profile";
-import Reports from "./pages/Reports";
-import Profile from "./pages/Profile";
+import Reports from "./pages/Report";
 import Budget from "./pages/Budget";
 import Settings from "./pages/Settings";
-import Dashboard from "./pages/DashBoard";
 
 function App() {
   return (
@@ -20,13 +18,8 @@ function App() {
       <Route path="/transactions" element={<Transactions />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/reports" element={<Reports />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route
-    path="/budgets"
-    element={<Budget />}
-/>
-    <Route path="/settings" element={<Settings />} />
+      <Route path="/budgets" element={<Budget />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   );
 }
