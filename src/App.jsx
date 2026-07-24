0d5d6ee86901853cb571e90438a919648fe89841
Mashood Alam <alammashood123@gmail.com>
2026-07-24 11:35:15 +0500

    Fix page imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Transactions from "./pages/Transactions";
+import Transactions from "./pages/Transactions";;
 import Profile from "./pages/Profile";
-import Reports from "./pages/Reports";
+import Reports from "./pages/Report"
 import Budget from "./pages/Budget";
 import Settings from "./pages/Settings";
 
