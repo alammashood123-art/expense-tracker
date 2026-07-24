e39b24e42b7f75344b4ce8ee9c184b4486fe1044
Mashood Alam <alammashood123@gmail.com>
2026-07-24 11:52:31 +0500

    Fix App imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Transaction from "./pages/Transactions";;
+import Transaction from "./pages/Transaction";
 import Profile from "./pages/Profile";
-import Reports from "./pages/Report"
+import Report from "./pages/Report";
 import Budget from "./pages/Budget";
 import Settings from "./pages/Settings";
 
@@ -17,7 +17,7 @@ function App() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/transactions" element={<Transaction />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/reports" element={<Reports />} />
+      <Route path="/reports" element={<Report />} />
       <Route path="/budgets" element={<Budget />} />
       <Route path="/settings" element={<Settings />} />
     </Routes>
